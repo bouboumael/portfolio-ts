5452959d2e1a7a3ccf9e789838655b2a30c46bbe
bouboumael <bouboumael@orange.fr>
2022-01-31 00:18:50 +0100

    animation and start typescript

--- src/components/homeparts/FirstView.tsx
+++ src/components/homeparts/FirstView.tsx
@@ -2,11 +2,10 @@ import React from "react";
 import logo from "../../assets/images/mael-logo-pins.png";
 
 function FirstView() {
+
     return (
-        <div className={"p-10 flex flex-col justify-center items-center text-center"}>
-            <img className={'mt-5'} src={logo} alt="logo en forme de pins de mael chariault"/>
-            <h1>Maël Chariault</h1>
-            <h2>Développeur web Full Satck</h2>
+        <div className={"p-20 flex flex-col justify-center items-center text-center firstview"}>
+            <img src={logo} alt={"Mael logo"} className={`mael-logo rotating`}/>
         </div>
     );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,26 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './navbar.scss';
+import {ToggleMenu} from "../../types/portfolioTypes";
 
 function Navbar() {
 
+    const [isShow, setIsShow] = useState<boolean>(false);
+    useEffect(() => {
+        toggleMenu();
+    },[])
+
+    const toggleMenu: ToggleMenu = () => {
+        setIsShow(!isShow);
+    }
+
     return (
         <>
-            <div className="navbar flex justify-center">
-                <ul id="menu">
-                    <a className="menu-button icon-plus" href="#" title="Afficher le Menu"/>
-                    <a className="menu-button icon-minus" href="#" title="Cacher le menu"/>
+            <div className={`navbar flex justify-center`}>
+                <ul id="menu" className={isShow ? 'isVisible' : 'notVisible'}>
+                    <a className="menu-button icon-plus" href="#menu" title="Afficher le Menu"/>
+                    <a className="menu-button icon-minus" href="#0" title="Cacher le menu"/>
                     <li className="menu-item">
                         <a href="#menu">
                             <span className="fab fa-github"/>
                         </a>
                     </li>
                     <li className="menu-item">
-                        <a href="#">
+                        <a href="#menu">
                             <span className="fas fa-envelope"/>
                         </a>
                     </li>
                     <li className="menu-item">
-                        <a href="#">
+                        <a href="#menu">
                             <span className="fas fa-id-card-alt"/>
                         </a>
                     </li>
