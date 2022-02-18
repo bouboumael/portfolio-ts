65cc664c51ef60b9afbb5e994f61b4eb93d7d280
bouboumael <bouboumael@orange.fr>
2022-02-18 17:14:12 +0100

    card left position

--- src/components/homeparts/FourthView.tsx
+++ src/components/homeparts/FourthView.tsx
@@ -1,43 +1,11 @@
 import React from 'react';
-import {CardSwiper} from "react-card-rotate-swiper";
-import {projectsCards} from "../../datas/projects";
-import {ProjectCard} from "../../types/portfolioTypes";
+import CarouselProjects from "../carousels/CarouselProjects";
 
 const FourthView = () => {
 
-    const cards = projectsCards
-
-    let indexInit: number = 100;
-    let positionLeft: number = 20.5;
-    cards.map((card: ProjectCard, index: number) => {
-        if (index > 0) {
-            const newIndex: number = Math.ceil(indexInit - (indexInit / cards.length));
-            card.style = {
-                zIndex: newIndex,
-                left: positionLeft + '%',
-            };
-            indexInit = indexInit - newIndex;
-            positionLeft = positionLeft + 0.2;
-        } else {
-            card.style = {zIndex: indexInit};
-        }
-        console.log(card)
-    })
-
     return (
         <div className={'p-20 fourthView'}>
-            <h2>Mes projets récents:</h2>
-            <div className="pt-10 mt-10 flex justify-center relative min-h-screen overflow-hidden">
-                <div className={'w-1/2 flex justify-center'}>
-                    {cards.map((card: ProjectCard, index: number) => {
-                        return (
-                            <div className={"absolute"} key={index} style={card.style}>
-                                <CardSwiper className={"card"} contents={card.element}/>
-                            </div>
-                        )
-                    })}
-                </div>
-            </div>
+            <CarouselProjects/>
         </div>
     )
 }
