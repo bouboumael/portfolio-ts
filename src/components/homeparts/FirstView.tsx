ac2eccffa8b24be3dc51ea892bfddf9284cce8ac
bouboumael <bouboumael@orange.fr>
2022-06-03 14:40:07 +0200

    arrow scroll invitation

--- src/components/homeparts/FirstView.tsx
+++ src/components/homeparts/FirstView.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LogoBall from "../logoBall/LogoBall";
+import DoubleArrow from "../doubleArrow/DoubleArrow";
 
 function FirstView() {
 
     return (
-        <div className={"flex items-center text-center firstview"}>
+        <div className={"flex flex-col justify-around items-center text-center firstview"}>
             <LogoBall />
+            <DoubleArrow />
         </div>
     );
 }
