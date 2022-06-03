36da464b9045d54c093fe5ada32fe7dc4d054fa0
bouboumael <bouboumael@orange.fr>
2022-06-03 15:36:26 +0200

    logo animation

--- src/components/logoBall/LogoBall.tsx
+++ src/components/logoBall/LogoBall.tsx
@@ -1,39 +1,34 @@
 import gsap from "gsap";
-import React, {useLayoutEffect, useRef, useState} from "react";
+import React, {useLayoutEffect, useRef} from "react";
 import logo from "../../assets/images/mael-logo-pins.png"
 
 
 const LogoBall = () => {
 
-    const [size, setSize] = useState<number>();
     let image = new Image()
     image.src = logo;
 
     const boxVar = {
         from: {
-            x: -500,
+            x: - document.body.clientWidth,
+            opacity: -1
         },
         to: {
             rotation: 360,
-            x: size,
-            duration: 2
+            x: 0,
+            duration: 2,
+            opacity: 1
         },
     }
 
     const boxRef = useRef(null);
     useLayoutEffect(() => {
         image.onload = () => {
-            if (document.body.clientWidth > 768) {
-                setSize((document.body.clientWidth - image.width) / 2)
-            } else {
-                setSize(0)
-            }
-
             gsap.fromTo(boxRef.current,boxVar.from, boxVar.to);
         }
     });
 
-    return <img src={logo} alt={"Mael logo"} className={`mt-60 md:mt-0 mael-logo`} ref={boxRef}/>
+    return <img src={logo} alt={"Mael logo"} className={`mt-60 md:mt-25 mael-logo`} ref={boxRef}/>
 }
 
 export default LogoBall;
